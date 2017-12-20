8da5325b876e8e350ebba5f7260b5dfe279a6f8e
Anton Herzog <info@antonherzog.com>
2017-12-20 23:48:42 +0100

    Finish Day01.

--- Day01/InverseCaptcha.fs
+++ Day01/InverseCaptcha.fs
@@ -7,9 +7,22 @@ module InverseCaptcha =
 
     let withTail list = Seq.last list :: list
 
-    let compute num = num
-                      |> digits
-                      |> withTail
-                      |> Seq.windowed 2
-                      |> Seq.filter(fun a -> a.[0] = a.[1])
-                      |> Seq.sumBy Seq.head 
+    let equalNeighbours : seq<int> -> seq<int[]> = Seq.windowed 2 >> Seq.filter(fun a -> a.[0] = a.[1])
+
+
+    let splitInHalfs (list : list<int>) = (List.take (list.Length/2) list, List.skip (list.Length/2) list)
+    let computeSameHalfWayArround num =
+        let (l,r)=num 
+                 |> digits 
+                 |> splitInHalfs
+
+        List.zip l r
+          |> Seq.filter (fun (x,y)-> x=y)
+          |> Seq.sumBy (fun (x,y)->x+y)
+
+    let computeSameNeighbourSum num = num
+                                      |> digits
+                                      |> withTail
+                                      |> Seq.windowed 2
+                                      |> Seq.filter(fun a -> a.[0] = a.[1])
+                                      |> Seq.sumBy Seq.head 
